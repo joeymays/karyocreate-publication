a07dfe2f673e3879726f22696d1a100a048d7257
Joey Mays <joeymays94@gmail.com>
2022-10-19 12:42:14 -0400

    Update helper-functions.R

--- scripts/helper-functions.R
+++ scripts/helper-functions.R
@@ -21,18 +21,3 @@ corner <- function(input.mat){
     
     return(input.mat[1:row.out, 1:col.out])
 }
-
-#twoArmPalette adapted from Blake R Mills's MetBrewer package, Cross palette.
-#https://github.com/BlakeRMills/MetBrewer
-twoArmPalette <- c("#FFB4AD", 
-                   "#CEA1CA",
-                   "#53939b",
-                   "#004d64",
-                   "#081f51",
-                   "#ffba00",
-                   "#fd6f00",
-                   "#e12f34",
-                   "#7e9a65")
-
-twoArmPaletteOrder <- c("loss_gain", "gain_loss", "neutral_loss", "loss_neutral", "loss_loss",
-                        "neutral_gain", "gain_neutral", "gain_gain", "neutral_neutral")
